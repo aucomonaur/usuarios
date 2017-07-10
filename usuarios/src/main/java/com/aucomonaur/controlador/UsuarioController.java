b9fc119c80e7b80be423a6ecff08b368bc0e6030
diego2945 <diegou.2945@gmail.com>
2017-07-09 21:48:12 -0500

    Terminada la interfaz de comunicación

--- usuarios/src/main/java/com/aucomonaur/controlador/UsuarioController.java
+++ usuarios/src/main/java/com/aucomonaur/controlador/UsuarioController.java
@@ -1,8 +1,12 @@
 package com.aucomonaur.controlador;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +21,30 @@ public class UsuarioController {
 	@Autowired
 	RepositorioUsuarios repositorioUsuarios;
 	
-	@GetMapping(value="/lista")
-	public List<Usuario> getAll(){
-		return repositorioUsuarios.findAll();
+	
+	@GetMapping(value="/get_usuario")
+	public ResponseEntity<List<Usuario>> getUsuario(String id_usuario){
+		List<Usuario> resultado = new ArrayList<>();
+		
+		//Si no esta bien estructurado, retornar bad request
+		if(!id_usuario.matches("^([0-9]+)(,[0-9]+)*$"))
+			return new ResponseEntity<List<Usuario>>(HttpStatus.BAD_REQUEST);
+		
+		String [] ids_usuarios = id_usuario.split(",");
+		
+		for (int i = 0; i < ids_usuarios.length; i++) {
+			resultado.add(repositorioUsuarios.findOne(Long.valueOf(ids_usuarios[i])));
+		}
+		
+		return new ResponseEntity<List<Usuario>>(resultado, HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/search_correo")
+	public ResponseEntity<Usuario> searchCorreo(String correo){
+		
+		Usuario usuario;
+		usuario = repositorioUsuarios.findByCorreo(correo);
+		
+		return new ResponseEntity<Usuario>(usuario,HttpStatus.OK);
 	}
 }
--- usuarios/src/main/java/com/aucomonaur/repositorio/RepositorioUsuarios.java
+++ usuarios/src/main/java/com/aucomonaur/repositorio/RepositorioUsuarios.java
@@ -6,5 +6,8 @@ import com.aucomonaur.modelo.Usuario;
 
 public interface RepositorioUsuarios extends JpaRepository<Usuario, Long>{
 	
+	//Consulta para buscar mediante correo
+	
+	Usuario findByCorreo(String correo);
 
 }
